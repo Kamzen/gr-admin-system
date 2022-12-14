df04b10c17aa92c37d8afaae3382a5e2d54e71a2
Kamzen <37660966+Kamzen@users.noreply.github.com>
2022-12-15 00:49:21 +0200

    created landing splash screen

---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DashboardCard from './DashboardCard'
+
+const Dashboard = () => {
+  return (
+    <DashboardCard title={'View Employees'} link={'/login'} />
+  )
+}
+
+export default Dashboard
