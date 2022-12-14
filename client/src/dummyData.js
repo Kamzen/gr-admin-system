200455ced86668d234406cd7dce392819e4c2cbe
Kamzen <37660966+Kamzen@users.noreply.github.com>
2022-12-15 00:47:37 +0200

    created dummy data

---
+++ client/src/dummyData.js
@@ -0,0 +1,76 @@
+const users = [
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+    {
+        uuid: '1',
+        firstName: 'Tiyisela Themba',
+        lastName: 'Makamu',
+        phoneNumber: '0797126016',
+        email: 'kamzen1998',
+        managerId: '2',
+        status: 'active'
+    },
+]
+
+export default users
